1af97159e663433412a8df9908bf8f736e3eb31f
yuo_ota <yuuta.tsunashima@gmail.com>
2025-06-18 19:06:25 +0900

    update

--- main.py
+++ main.py
@@ -12,27 +12,6 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
-@app.get("/omikuji")
-def omikuji():
-    omikuji_list = [
-        "大吉",
-        "中吉",
-        "小吉",
-        "吉",
-        "半吉",
-        "末吉",
-        "末小吉",
-        "凶",
-        "小凶",
-        "大凶"
-    ]
-    
-    return {"result" : omikuji_list[random.randrange(10)]}
-
 @app.get("/index")
 def index():
     html_content = """
